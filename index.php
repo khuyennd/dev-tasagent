83e557406900b1a8cc4fa4fcf2a432347c3bfc8e
chienvuanh <chienvuanh@gmail.com>
2015-10-01 15:07:01 +0700

    commit echo 111

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 * 2012 TAS
 */
 define('IN_TAS', true);
-echo 111;
-die();
+//echo 111;
+//die();
 require(dirname(__FILE__).'/config/config.inc.php');
 // $smarty->testInstall();
 ControllerFactory::getController('IndexController')->run();
